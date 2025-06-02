f322a188cbfd39c922fbc1dbe916dea23e6538ec
kimu-shun <ttc2350sg0021@edu.tech.ac.jp>
2025-06-02 17:00:14 +0900

    2DActioinGame

---
+++ 2DActioinGame/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-2DActionGame-1.0";
+const contentToCache = [
+    "Build/2DActioinGame.loader.js",
+    "Build/2DActioinGame.framework.js.unityweb",
+    "Build/2DActioinGame.data.unityweb",
+    "Build/2DActioinGame.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
